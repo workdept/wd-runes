cd8c1bd122452698326293d05a6f7cbdb84598f0
Benjamin Chodoroff <bc@phy3.thermitic.net>
2012-05-14 17:59:33 -0400

    add simple http server to change a random rune

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // runeserver
 
-var io = require('socket.io').listen(31337);
+var http = require('http'),
+    io   = require('socket.io').listen(31337);
 
 // make an initial large list of images to display
 var imgs = ['smaller-bolt_hot.png', 'smaller-burst_hot.png', 'smaller-circles_hot.png', 'smaller-xchange_hot.png', 'smaller-bolt.png', 'smaller-burst.png', 'smaller-circles.png', 'smaller-xchange.png'];
@@ -29,4 +30,16 @@ io.sockets.on('connection', function(socket) {
 
 });
 
+// run a one-trick-pony to change a random rune
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  var index = Math.floor(Math.random() * 8);
+  imgs_state[index] = imgs[Math.floor(Math.random() * imgs.length)];
+  io.sockets.emit('runechange', {
+    index: index,
+    newrune: imgs_state[index]
+  });
+
+  res.end('Rune ' + index + ' changed');
+}).listen(31336);
 
